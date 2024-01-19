e09b27c4d95509da06d993785a41aed4f5207cd4
Evan Ribey <contact@evanribey.com>
2024-01-19 17:16:02 -0500

    add root import refencing in compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
   plugins: [solid()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 })
