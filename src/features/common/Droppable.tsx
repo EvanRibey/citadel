9cdbbde68452e2ee61887f703b49503ffdcb128e
Evan Ribey <contact@evanribey.com>
2024-01-19 23:03:38 -0500

    refine droppable component variables

--- src/features/common/Droppable.tsx
+++ src/features/common/Droppable.tsx
@@ -2,21 +2,13 @@ import { JSX } from 'solid-js';
 import { DroppableProps } from './types';
 
 export function Droppable(props: DroppableProps): JSX.Element {
-  const handleDrop = () => {
-    props.onDrop();
-  };
-
-  const handleDragOver = () => {
-    props.onDragOver();
-  };
-
-  const conditionalDrop = () => props.isDroppable ? handleDrop : undefined;
-  const conditionalDragOver = () => props.isDroppable ? handleDragOver : undefined;
+  const handleDrop = () => props.isDroppable ? props.onDrop : undefined;
+  const handleDragOver = () => props.isDroppable ? props.onDragOver : undefined;
 
   return (
     <div
-      onDrop={conditionalDrop}
-      onDragOver={conditionalDragOver}
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
     >
       {props.children}
     </div>
