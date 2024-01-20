bb059f875dcc31036e5427e61d32de285d20c52a
Evan Ribey <contact@evanribey.com>
2024-01-19 23:08:38 -0500

    fix linting issues

--- src/features/card-game/Tableau.tsx
+++ src/features/card-game/Tableau.tsx
@@ -3,7 +3,7 @@ import './Tableau.css';
 function Tableau() {
   return (
     <div class="card-game-tableau">
-      <div class="card-pile"></div>
+      <div class="card-pile"/>
       <div class="foundation"/>
       <div class="card-pile"/>
       <div class="card-pile"/>
