163a744a53f76118c3698e405088a49f7541f2db
Evan Ribey <contact@evanribey.com>
2024-01-19 22:34:25 -0500

    testing again

--- src/features/card-game/Tableau.tsx
+++ src/features/card-game/Tableau.tsx
@@ -3,9 +3,9 @@ import './Tableau.css';
 function Tableau() {
   return (
     <div class="card-game-tableau">
-      <div class="card-pile" ></div>
+      <div class="card-pile"></div>
       <div class="foundation" />
-      <div class="card-pile" />
+      <div class="card-pile"></div>
       <div class="card-pile" />
       <div class="foundation" />
       <div class="card-pile" />
