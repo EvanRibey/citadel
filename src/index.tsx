2d97c633df76c9570b4df05c6b2be0091610cd9f
Evan Ribey <contact@evanribey.com>
2024-01-19 20:32:30 -0500

    move files before initial development

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@
 import { render } from 'solid-js/web';
 
 import './index.css';
-import App from './App';
+import App from '@/app/App.tsx';
 
-const root = document.getElementById('root',);
+const root = document.getElementById('root');
 
-render(() => <App />, root!,);
+render(() => <App />, root!);
