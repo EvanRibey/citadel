5147cc23c664679f8c91cb8c2c89f6d18d0788f9
Evan Ribey <contact@evanribey.com>
2024-02-16 12:42:20 -0500

    Cleanup: remove unused Card type

--- src/common/types/index.ts
+++ src/common/types/index.ts
@@ -3,10 +3,4 @@ export type Direction = 'rtl' | 'ltr';
 export type Suit = 'diamonds' | 'hearts' | 'spades' | 'clubs';
 export type CardValue = 'Ace' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'Jack' | 'Queen' | 'King';
 
-export interface Card {
-  value: CardValue,
-  suit: Suit,
-  image?: string,
-}
-
 export * from './dnd';
